c41190c682707546d48cde3c6d542d159c5628a7
sinatb <sinatb.dev@gmail.com>
2023-09-18 21:13:51 +0330

    added broken rotating

--- Assets/Scripts/Player/Input.cs
+++ Assets/Scripts/Player/Input.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -8,6 +7,12 @@ public class Input : MonoBehaviour
     private Rigidbody2D _rb;
     private Player _p;
     private Vector2 _dir;
+    private Vector3 _mousepos;
+    private Vector2 _target;
+    private Vector2 _rotation;
+    
+    [SerializeField]
+    private Camera _maincam;
     private void Awake()
     {
         _rb = gameObject.GetComponent<Rigidbody2D>();
@@ -17,11 +22,16 @@ public class Input : MonoBehaviour
     {
         var horizontal = UnityEngine.Input.GetAxis("Horizontal");
         var vertical = UnityEngine.Input.GetAxis("Vertical");
+        _mousepos = _maincam.WorldToScreenPoint(UnityEngine.Input.mousePosition);
+        _mousepos.z = 0;
+        _target = _mousepos - transform.position;
+        _rotation = Vector3.RotateTowards(transform.position,_target,)
         _dir = new Vector2(horizontal, vertical).normalized;
     }
 
     private void FixedUpdate()
     {
         _rb.MovePosition((Vector2)transform.position + _dir * (_p.Speed * Time.deltaTime));
+        _rb.MoveRotation(Vector2.Angle((_mousepos - transform.position),Vector2.right));
     }
 }
