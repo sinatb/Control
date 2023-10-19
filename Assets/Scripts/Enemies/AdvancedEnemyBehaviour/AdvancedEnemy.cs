90e86b9b07d4599d255aa1c6fada678104c64615
sinatb <sinatb.dev@gmail.com>
2023-10-19 10:59:40 +0330

    basic functions of advanced enemy added

--- Assets/Scripts/Enemies/AdvancedEnemyBehaviour/AdvancedEnemy.cs
+++ Assets/Scripts/Enemies/AdvancedEnemyBehaviour/AdvancedEnemy.cs
@@ -1,29 +1,84 @@
+using System;
 using UnityEngine;
-
+using Random = UnityEngine.Random;
 public class AdvancedEnemy : MonoBehaviour,INCUnit,IEnemy
 {
-    public float Speed { get; set; }
-    public float Health { get; set; }
-    public Vector2 Direction { get; set; }
-    public float Damage { get; set; }
+    [SerializeField] private float health;
+    [SerializeField] private float speed;
+    [SerializeField] private float damage;
+    [SerializeField] private float range;
+    private Rigidbody2D _rb;
+    private Collider2D _col;
+    private Vector2 _direction;
+    public float Health
+    {
+        get => health;
+        set => health = value;
+    }
+    public float Speed
+    {
+        get => speed;
+        set => speed = value;
+    }
+
+    public Vector2 Direction
+    {
+        get => _direction;
+        set => _direction = value;
+    }
+    public float Damage
+    {
+        get => damage;
+        set => damage = value;
+    }
+
+    private void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+        _col = gameObject.GetComponent<CircleCollider2D>();
+        //give a random direction for the enemy to move at the start
+        var rndx = Random.Range(0.0f, 1.0f);
+        var rndy = Random.Range(0.0f, 1.0f);
+        _direction = new Vector2(rndx, rndy).normalized;
+    }
 
     public void PlanMove()
     {
-        throw new System.NotImplementedException();
+        for (int i = 0; i < 360; i++)
+        {
+            Quaternion q = Quaternion.AngleAxis(i, Vector3.forward);
+            var direction = Vector3.up;
+            direction = q * direction;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction,range);
+            if (!hit) continue;
+            if (hit.transform.CompareTag("Player"))
+            {
+                _direction = direction;
+            }
+        }
     }
 
     public void Move()
     {
-        throw new System.NotImplementedException();
+        _rb.MovePosition((Vector2)transform.position + _direction*(Time.deltaTime*speed));
     }
 
     public void Attack(Player p)
     {
         throw new System.NotImplementedException();
     }
-
     public void ReceiveDamage(float amount)
     {
-        throw new System.NotImplementedException();
+        health -= amount;
+    }
+    private void Update()
+    {
+        if (health <= 0)
+            Destroy(gameObject);
+        PlanMove();
+    }
+    private void FixedUpdate()
+    {
+        Move();
     }
 }
