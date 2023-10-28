60f85bdafd26f346c13d5b53b364f3818de17252
sinatb <sinatb.dev@gmail.com>
2023-10-28 14:58:28 +0330

    added basic player boost

---
+++ Assets/Scripts/Player/PlayerBoost.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBoost : MonoBehaviour
+{
+    private Player _currPlayer;
+    private bool _isBoosting;
+    private void Start()
+    {
+        _currPlayer = GetComponent<Player>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftShift) && !_isBoosting) 
+        {
+            _isBoosting = true;
+            StartCoroutine(BoostCalculator());           
+        }
+    }
+    private IEnumerator BoostCalculator() 
+    {
+        float time_elapsed = 0.0f;
+        for (int i = 0; i<50; i++)
+        {
+            if (i < 10)
+                _currPlayer.Speed += 0.5f;
+            else
+                _currPlayer.Speed -= 0.125f;
+            yield return new WaitForSeconds(0.01f);
+            time_elapsed += 0.01f;
+        }
+
+        _isBoosting = false;
+    }
+}
