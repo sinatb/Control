47397b3e0b6ed20882dedc1c4754f598648e1e57
sinatb <sinatb.dev@gmail.com>
2023-10-30 10:13:41 +0330

    rage added without rotation

--- Assets/Scripts/Player/PlayerRage.cs
+++ Assets/Scripts/Player/PlayerRage.cs
@@ -1,29 +1,52 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 [RequireComponent(typeof(PlayerShoot),typeof(PlayerBoost),typeof(Player))]
 public class PlayerRage : MonoBehaviour
 {
     [SerializeField] private float RageTime;
     [SerializeField] private GameObject Gun;
+    [SerializeField] private GameObject Bullet;
+    private List<Vector2> ShootingPos;
     public delegate void OnRageEnd();
     public static event OnRageEnd onRageEnd;
+    private void Awake()
+    {
+        ShootingPos = new List<Vector2>
+        {
+            new Vector2(1.0f,0.0f).normalized,
+            new Vector2(-1.0f,0.0f).normalized,
+            new Vector2(0.0f,1.0f).normalized,
+            new Vector2(0.0f,-1.0f).normalized,
+            new Vector2(1.0f,1.0f).normalized,
+            new Vector2(1.0f,-1.0f).normalized,
+            new Vector2(-1.0f,1.0f).normalized,
+            new Vector2(-1.0f,-1.0f).normalized
+        };
+    }
     private void InitRage()
     {
         GetComponent<PlayerShoot>().enabled = false;
         GetComponent<PlayerBoost>().enabled = false;
         Gun.SetActive(false);
-        Debug.Log("Rage init done");
     }
     private void RageShoot()
     {
-        Debug.Log("Shoot");
+        var position = transform.position;
+        foreach(var x in ShootingPos)
+        {
+            var angle = Mathf.Atan2(x.x, x.y) * Mathf.Rad2Deg;
+            Instantiate(Bullet,
+                (Vector2)position + x,
+                Quaternion.Euler(new Vector3(0.0f, 0.0f, -angle))
+                );
+        }
     }
     private void EndRage() 
     {
         GetComponent<PlayerShoot>().enabled = true;
         GetComponent<PlayerBoost>().enabled = true;
         Gun.SetActive(true);
-        Debug.Log("Rage Done");
     }
     public IEnumerator Rage()
     {
@@ -31,7 +54,6 @@ public class PlayerRage : MonoBehaviour
         for (float i=0; i < RageTime; i += 0.1f) 
         {
             RageShoot();
-            print(i);
             yield return new WaitForSeconds(0.1f);
         }
         EndRage();
